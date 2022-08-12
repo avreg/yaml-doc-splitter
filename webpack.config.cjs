37be03ae41358db94a232f3ba9bc963fe2395ac1
Andrey Nikitin <support@avreg.net>
2022-08-12 13:53:38 +0300

    build: remove useless (webpack5?) CleanWebpackPlugin

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 const path = require('path')
 
@@ -46,21 +45,6 @@ module.exports = (env, argv) => {
       optimization: {
          minimize: isProductionBuild,
          minimizer: [new TerserPlugin()]
-      },
-
-      plugins: [
-         /**
-          * All files inside webpack's output.path directory will be removed once, but the
-          * directory itself will not be. If using webpack 4+'s default configuration,
-          * everything under <PROJECT_DIR>/dist/ will be removed.
-          * Use cleanOnceBeforeBuildPatterns to override this behavior.
-          *
-          * During rebuilds, all webpack assets that are not used anymore
-          * will be removed automatically.
-          *
-          * See `Options and Defaults` for information
-          */
-         new CleanWebpackPlugin()
-      ]
+      }
    }
 }
